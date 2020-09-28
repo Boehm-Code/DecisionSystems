8b8f1fedc4825635315b2410b53ec06971d4a683
Nikolaus Boehm <nikolaus.boehm@htlvb.at>
2020-09-28 16:15:25 +0200

    Added the GetDistanceMethod in Utils

--- DecisionSystems/TSP/Utils.cs
+++ DecisionSystems/TSP/Utils.cs
@@ -8,7 +8,26 @@ namespace DecisionSystems.TSP
     {
         public static double GetDistance(IReadOnlyCollection<int> solution, IReadOnlyList<Location> cities)
         {
-            return 0;
+
+            var lastCity = solution.Last();
+            var distance = 0.0;
+
+            foreach (var position in solution)
+            {
+                var previousCity = cities[lastCity - 1];
+                var city = cities[position - 1];
+
+                distance += GetDistance(previousCity, city);
+                lastCity = position;
+            }
+
+            return distance;
         }
+
+        private static double GetDistance(Location location1, Location location2)
+        {
+            return Math.Sqrt(Math.Pow(location2.X - location1.X, 2) + Math.Pow(location2.Y-location1.Y,2));
+        }
+        
     }
 }
